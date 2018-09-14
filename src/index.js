7354ae887187af5656c2d5d2fcac19846fa4db9f
bryanshrugged <bsummerscon@merkleinc.com>
2018-09-14 14:11:17 -0600

    Added basic parsing

--- src/index.js
+++ src/index.js
@@ -1,25 +1,61 @@
 'use strict';
 
 const axios = require('axios');
-const pdfParser = require('pdf-parser');
-const PDFParser = require("pdf2json");
+const PDFParser = require('pdf2json');
 const fs = require('fs');
 
-const parser = new PDFParser();
+// District calendars
+const districtCalendars = ['MANTI', 'MOAB', 'SLC', 'WEST_JORDAN', 'OGDEN'];
 
-const calendars = [
-    'MANTI',
-    'MOAB'
+// Justice Court Calendars
+const justiceCalendars = [
+  'Just_HEBER_CITY_2607',
+  'Just_Draper_1823',
+  'Just_South_Jordan_1878',
+  'Just_SALT_LAKE_CITY_1867',
+  'Just_SANDY_1873',
+  'Just_Taylorsville_1881',
+  'Just_Herriman_1833'
 ];
 
-parser.on("pdfParser_dataError", errData => console.error(errData.parserError) );
-parser.on("pdfParser_dataReady", pdfData => {
-    fs.writeFileSync('./src/pdfs/MOAB.json', JSON.stringify(pdfData));
-});
+const run = async _calendar => {
+  const parser = new PDFParser();
+  parser.on('pdfParser_dataError', errData => console.error(errData.parserError));
+  parser.on('pdfParser_dataReady', pdfData => {
+    const pageLines = [];
+    const formattedPages = [];
+    const allPages = pdfData['formImage']['Pages'];
+    for (let i = 0; i < allPages.length; i++) {
+      const Page = allPages[i];
+      Page['Texts'] = Page['Texts'].map(_TEXT => {
+        return _TEXT['R'].map(_R => {
+          _R['T'] = _R['T'].replace(/%20/g, ' ');
+          pageLines.push(_R['T']);
+          return _R;
+        });
+      });
+      formattedPages.push(Page);
+    }
+    for (let j = 0; j < formattedPages.length; j++) {
+      const pg = formattedPages[j];
+      pg.PageText = pg['Texts'].map(_text => _text[0]['T']);
+      formattedPages[j] = pg;
+    }
+    fs.writeFileSync(
+      `./src/pdfs/${_calendar}.${new Date().getMonth()}-${new Date().getDay()}-${new Date().getFullYear()}.json`,
+      JSON.stringify(pdfData)
+    );
+  });
+  const response = await axios.get(`https://www.utcourts.gov/cal/data/${_calendar}_Calendar.pdf`, {
+    responseType: 'arraybuffer'
+  });
+  parser.parseBuffer(Buffer.from(response.data));
+};
 
-const run = async () => {
-    const response = await axios.get('https://www.utcourts.gov/cal/data/MOAB_Calendar.pdf', { responseType: 'arraybuffer' });
-    parser.parseBuffer(Buffer.from(response.data));
+for (let i = 0; i < districtCalendars.length; i++) {
+  run(districtCalendars[i]);
 }
 
-run();
+for (let k = 0; k < justiceCalendars.length; k++) {
+  run(justiceCalendars[k]);
+}
